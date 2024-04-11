2686a665e4f947278fd3195f3d58bb051ef5870d
harold alzate <99224461+harold12a@users.noreply.github.com>
2024-04-10 19:49:36 -0500

    Update JpaprojectionsApplicationTests.java

--- jpaprojections-main/src/test/java/com/jaax/jpaprojections/JpaprojectionsApplicationTests.java
+++ jpaprojections-main/src/test/java/com/jaax/jpaprojections/JpaprojectionsApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class JpaprojectionsApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+   void contextLoads() {
+        assertThat(myService).isNotNull(); // Verifica que el bean MyService no sea nulo
+    }
 }
